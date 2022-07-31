52b83caea96b5206ce58731e3f15b393643f4c61
benjamin <b.james.neustadt@gmail.com>
2022-08-01 01:56:21 +0200

    Add: useState
    * import from react
    * set the array of objects inside the useState

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,25 +1,28 @@
-const tasks = [
-  {
-    id: 1,
-    text: 'Doctors Appointment',
-    day: 'Feb 5th at 2:30pm',
-    reminder: true,
-  },
-  {
-    id: 2,
-    text: 'Meeting at School',
-    day: 'Feb 3th at 09:30am',
-    reminder: true,
-  },
-  {
-    id: 3,
-    text: 'Swimming lesson',
-    day: 'Feb 7th at 12:30pm',
-    reminder: false,
-  }
-]
+import { useState } from 'react'
+
 
 const Tasks = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Doctors Appointment',
+      day: 'Feb 5th at 2:30pm',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Meeting at School',
+      day: 'Feb 3th at 09:30am',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'Swimming lesson',
+      day: 'Feb 7th at 12:30pm',
+      reminder: false,
+    }
+  ])
+
   return (
     <>
       {tasks.map((task) => (<h3 key={task.id}>{task.text}</h3>))}
@@ -28,3 +31,4 @@ const Tasks = () => {
 }
 
 export default Tasks
+
