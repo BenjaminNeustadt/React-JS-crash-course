dfbbb06a3bc936c8d9e9b81d575443a73b9a46b0
benjamin <b.james.neustadt@gmail.com>
2022-08-01 02:02:06 +0200

    Update: change function syntax
    * l.5 in App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 
-function App() {
+const App = () => {
   const [tasks, setTasks] = useState([
     {
       id: 1,
