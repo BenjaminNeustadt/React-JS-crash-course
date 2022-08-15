034ed8fed21479d0216a2898f8a02c257472ac6e
benjamin <b.james.neustadt@gmail.com>
2022-08-15 23:12:21 +0200

    Modify: syntax in tasks.js

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -3,7 +3,10 @@ import Task from "./Task"
 const Tasks = ({tasks, onDelete}) => {
   return (
     <>
-      {tasks.map((task) => (<Task key={task.id} task={task} onDelete={onDelete}/>))}
+      {tasks.map((task) => (
+        <Task key={task.id} task={task} 
+        onDelete={onDelete}/>
+      ))}
     </>
   )
 }
