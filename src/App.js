ddda198aafef195b04f4481591e062350cbaf9b1
benjamin <b.james.neustadt@gmail.com>
2022-08-15 22:57:30 +0200

    Update: removed commented out code in App.js
    * 15 August, taking up from where we left off (52:35 seconds in video)

--- src/App.js
+++ src/App.js
@@ -38,25 +38,3 @@ const App = () => {
 }
 
 export default App;
-
-const App = () => {
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: 'Doctors Appointment',
-      day: 'Feb 5th at 2:30pm',
-      reminder: true,
-    },
-    {
-      id: 2,
-      text: 'Meeting at School',
-      day: 'Feb 3th at 09:30am',
-      reminder: true,
-    },
-    {
-      id: 3,
-      text: 'Swimming lesson',
-      day: 'Feb 7th at 12:30pm',
-      reminder: false,
-    }
-  ])
