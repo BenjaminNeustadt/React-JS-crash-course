465c2847fb921bfa8982cb2efd6d30b1360ae394
benjamin <b.james.neustadt@gmail.com>
2022-08-16 01:43:10 +0200

    Modifiy: Moved initial tasks into db.json

--- src/App.js
+++ src/App.js
@@ -5,26 +5,7 @@ import AddTask from './components/AddTask'
 
 const App = () => {
   const [showAddTask, setShowAddTask] = useState(true)
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: 'Doctors Appointment',
-      day: 'Feb 5th at 2:30pm',
-      reminder: true,
-    },
-    {
-      id: 2,
-      text: 'Meeting at School',
-      day: 'Feb 3th at 09:30am',
-      reminder: true,
-    },
-    {
-      id: 3,
-      text: 'Swimming lesson',
-      day: 'Feb 7th at 12:30pm',
-      reminder: false,
-    }
-  ])
+  const [tasks, setTasks] = useState([])
 
   // Add Task
   const addTask = (task) => {
