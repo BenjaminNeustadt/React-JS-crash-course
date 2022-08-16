cb773febe1353587e7facf28ed57932265cd2048
benjamin <b.james.neustadt@gmail.com>
2022-08-16 17:07:32 +0200

    Modify: styling for the placeholder text for input

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -24,12 +24,12 @@ const AddTask = ({ onAdd }) => {
     <form className="add-form" onSubmit={onSubmit}>
       <div className='form-control'>
         <label>Task</label>
-        <input type='text' placeholder="Add Task" value={text} onChange={(e) =>
+        <input class="input-text" type='text' placeholder="Add Task" value={text} onChange={(e) =>
         setText(e.target.value)}/>
       </div>
       <div className='form-control'>
         <label>Day & Time</label>
-        <input type='text' placeholder="Add Day & Time" value={day} onChange={(e) =>
+        <input class="input-text" type='text' placeholder="Add Day & Time" value={day} onChange={(e) =>
         setDay(e.target.value)}/>
       </div>
       <div className='form-control
