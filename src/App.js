1b19a1fcd5bd8611d72c186a0c51b38af9799e64
benjamin <b.james.neustadt@gmail.com>
2022-08-16 02:38:49 +0200

    Fixed: typo mix between fetch Tasks and Task, remider persists after refresh because comes from rest API

--- src/App.js
+++ src/App.js
@@ -23,6 +23,14 @@ const App = () => {
     return data
   }
 
+  // Fetch Task
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`)
+    const data = await res.json()
+
+    return data
+  }
+
   // Add Task
   const addTask = async (task) => {
     const res = await fetch('http://localhost:5000/tasks', {
@@ -44,16 +52,32 @@ const App = () => {
 
   // Delete Task
   const deleteTask = async (id) => {
-    await fetch(`http://localhost:5000/tasks/${id}`, { method: 'DELETE'})
+    await fetch(`http://localhost:5000/tasks/${id}`, { 
+      method: 'DELETE'
+    })
+
     setTasks(tasks.filter((task) => task.id !== id))
   }
 
   // Toggle reminder
-  const toggleReminder = (id) => {
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id)
+    const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(updTask)
+    })
+
+    const data = await res.json()
+
     setTasks(
       tasks.map((task) => 
         task.id === id ? {...task, reminder:
-        !task.reminder } : task
+        data.reminder } : task
       )
     )
   }
